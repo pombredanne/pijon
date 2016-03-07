719f3fc5daf5f39e3e21ba15e8c642fb3567870c
Thibaut Havel <thibaut.havel@surycat.com>
2016-03-07 11:49:24 +0100

    Delay module loading (optional).

--- pijon/pijon.py
+++ pijon/pijon.py
@@ -5,7 +5,7 @@ import os
 import re
 
 
-__all__ = ['Pijon', 'migrate']
+__all__ = ['Pijon', 'migrate', 'list', 'last']
 log = logging.getLogger(__name__)
 
 
@@ -14,25 +14,41 @@ def migrate(data, in_place=False, folder=None):
     return p.migrate(data, in_place=in_place)
 
 
+def list(folder=None):
+    p = Pijon(folder, load=False)
+    return p.migrations
+
+
+def last(folder=None):
+    p = Pijon(folder, load=False)
+    return p.last_migration
+
+
+Migration = collections.namedtuple(
+    'Migration',
+    ['ident', 'name', 'filename', 'module']
+)
+
+
 class Pijon(object):
 
     DEFAULT_FOLDER = 'migrations'
     MIGRATION_REGEX = r'(?P<ident>\d{4})_(?P<name>\w+)\.py'
 
-    def __init__(self, folder=None):
+    def __init__(self, folder=None, load=True):
         self.folder = folder or self.DEFAULT_FOLDER
-        self.migrations = self.fetch_migrations()
+        self.migrations = self.fetch(load)
 
     @property
     def last_migration(self):
         """
-        Returns last migration identity number
+        Returns last migration identity number.
         """
         return int(next(reversed(self.migrations))) if self.migrations else 0
 
-    def fetch_migrations(self):
+    def fetch(self, load=True):
         """
-        Return the `migrations` list
+        Fetch and load the migrations list (as Python modules).
         """
         migrations = collections.OrderedDict()
 
@@ -40,7 +56,7 @@ class Pijon(object):
             listdir = sorted(os.listdir(self.folder))
         except FileNotFoundError:
             os.mkdir(self.folder)
-            log.info('Created migration folder at %s', self.folder)
+            log.info("Created migration folder at {}".format(self.folder))
             return migrations
 
         for filename in listdir:
@@ -52,26 +68,30 @@ class Pijon(object):
             ident = match.group('ident')
             name = match.group('name')
 
-            # Load module
-            module = machinery.SourceFileLoader(
-                name, os.path.join(self.folder, filename)
-            ).load_module(name)
+            # Auto-load the module for further use
+            module = self._load(name, self.folder, filename) if load else None
 
-            migrations[ident] = module
+            migrations[ident] = Migration(ident, name, filename, module)
             log.debug("Fetching migration {} ({})".format(ident, name))
 
         return migrations
 
-    def _run_migrations(self, data, target=None):
+    @staticmethod
+    def _load(name, folder, filename):
+        return machinery.SourceFileLoader(
+            name, os.path.join(folder, filename)
+        ).load_module(name)
+
+    def _run(self, data, target=None):
         """
-        Run migrations on `data` until `target` version is reached
+        Run migrations on `data` until `target` version is reached.
         """
         initial_version = data.get('version', 0)
-        for ident, module in self.migrations.items():
-            migration_version = int(ident)
+        for ident, migration in self.migrations.items():
+            version = int(ident)
 
             # Skip if migration is past
-            if migration_version <= initial_version:
+            if version <= initial_version:
                 log.debug("Ignoring migration {}".format(ident))
                 continue
 
@@ -80,11 +100,14 @@ class Pijon(object):
                 log.info("Migration target ({}) is reached".format(target))
                 break
 
+            module = migration.module or self._load(
+                migration.name, self.folder, migration.filename
+            )
             log.info("Applying migration {}".format(ident))
             module.migrate(data)
-            data['version'] = migration_version
+            data['version'] = version
 
         return data
 
     def migrate(self, data, in_place=False, target=None):
-        return self._run_migrations(data if in_place else data.copy(), target)
+        return self._run(data if in_place else data.copy(), target)
